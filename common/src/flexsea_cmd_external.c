2e0ec02c5a477d5133fb9785eba1c45369389e74
jfduval <jfduval@ca98db78-25f0-4961-890e-003d10304561>
2015-02-19 19:14:30 +0000

    Adding 6 new pairs of .c/.h files that will eventually contain all the commands that are currently in rx_cmd and tx_cmd.
    git-svn-id: https://svn.media.mit.edu/r/biomech-ee/Code/flexsea_1_0@354 ca98db78-25f0-4961-890e-003d10304561

---
+++ common/src/flexsea_cmd_external.c
@@ -0,0 +1,60 @@
+//****************************************************************************
+// MIT Media Lab - Biomechatronics
+// Jean-Francois (Jeff) Duval
+// jfduval@mit.edu
+// 02/2015
+//****************************************************************************
+// flexsea_cmd_external: External/Expansion Commands
+//****************************************************************************
+
+//****************************************************************************
+// Include(s)
+//****************************************************************************
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../inc/flexsea.h"
+#include "flexsea_local.h"
+
+//Plan boards only:
+#ifdef BOARD_TYPE_FLEXSEA_PLAN
+//...
+#endif	//BOARD_TYPE_FLEXSEA_PLAN
+
+//Manage boards only:
+#ifdef BOARD_TYPE_FLEXSEA_MANAGE
+//...
+#endif	//BOARD_TYPE_FLEXSEA_MANAGE
+
+//Execute boards only:
+#ifdef BOARD_TYPE_FLEXSEA_EXECUTE
+#include "main.h"
+#endif	//BOARD_TYPE_FLEXSEA_EXECUTE
+
+//****************************************************************************
+// Local variable(s)
+//****************************************************************************
+
+
+//****************************************************************************
+// External variable(s)
+//****************************************************************************
+
+#ifdef BOARD_TYPE_FLEXSEA_EXECUTE
+
+#endif	//BOARD_TYPE_FLEXSEA_EXECUTE
+
+#ifdef BOARD_TYPE_FLEXSEA_MANAGE
+
+#endif	//BOARD_TYPE_FLEXSEA_MANAGE
+
+#ifdef BOARD_TYPE_FLEXSEA_PLAN
+
+#endif	//BOARD_TYPE_FLEXSEA_PLAN
+
+//****************************************************************************
+// Function(s)
+//****************************************************************************
+
+
+
