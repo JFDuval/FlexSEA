406ca730aaa47eef733400e64d909351d8cf4610
jfduval <jfduval@ca98db78-25f0-4961-890e-003d10304561>
2015-04-15 15:19:05 +0000

    git-svn-id: https://svn.media.mit.edu/r/biomech-ee/Code/flexsea_1_0@472 ca98db78-25f0-4961-890e-003d10304561

--- plan/src/shared.c
+++ plan/src/shared.c
@@ -4,60 +4,40 @@
 // jfduval@mit.edu
 // 04/2015
 //****************************************************************************
-// main: FlexSEA Plan project: console app to control FlexSEA slaves
+// shared: Code shared by Test, Demo, User functions, etc.
 //****************************************************************************
-
-#ifndef INC_MAINH_H
-#define INC_MAINH_H
 
 //****************************************************************************
 // Include(s)
 //****************************************************************************
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <time.h>
-#include "flexsea.h"
-#include "flexsea_console.h"
-#include "flexsea_stream_log.h"
-#include "plan_spi.h"
-#include "plan_serial.h"
-#include "flexsea_local.h"
-#include "user_shuobot.h"
+#include "main.h"
 #include "shared.h"
-#include "demo.h"
-#include "test.h"
 
 //****************************************************************************
-// Shared variable(s)
+// Local variable(s)
 //****************************************************************************
 
-extern char *fake_argv[];
-extern const char *delims;
-
 //****************************************************************************
-// Public Function Prototype(s):
+// Private Function Prototype(s):
 //****************************************************************************
 
 
-
 //****************************************************************************
-// Definition(s):
+// Function(s)
 //****************************************************************************
 
-#ifdef SINGLE_COMMAND
-#ifdef MULTIPLE_COMMANDS
-#error "Pick one Command option!"
-#endif
-#endif
+void braking_sequence(int cycles, int delay)
+{
+	int i = 0;
 
-//Multiple commands:
-#define MAX_COMMAND_LEN 		256
-#define MAX_ARGS 				12
-
-
-#endif
+	for(i = 0; i < cycles; i++)
+	{
+		tx_cmd_ctrl_o_write(FLEXSEA_EXECUTE_1, -400);
+		send_cmd_slave();
+		usleep(delay);
+		tx_cmd_ctrl_o_write(FLEXSEA_EXECUTE_1, 0);
+		send_cmd_slave();
+		usleep(delay);
+	}
+}
