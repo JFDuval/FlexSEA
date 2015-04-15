5270b966cb11d4acefc9210fdc0bbf32f5f7cba3
jfduval <jfduval@ca98db78-25f0-4961-890e-003d10304561>
2015-04-15 14:47:04 +0000

    git-svn-id: https://svn.media.mit.edu/r/biomech-ee/Code/flexsea_1_0@468 ca98db78-25f0-4961-890e-003d10304561

--- plan/inc/serial_port_driver.h
+++ plan/inc/serial_port_driver.h
@@ -2,53 +2,31 @@
 // MIT Media Lab - Biomechatronics
 // Jean-Francois (Jeff) Duval
 // jfduval@mit.edu
-// 04/2015
+// 08/2014
 //****************************************************************************
-// main: FlexSEA Plan project: console app to control FlexSEA slaves
+// usbtool_serial: Serial port driver
 //****************************************************************************
 
-#ifndef INC_MAINH_H
-#define INC_MAINH_H
+#ifndef INC_USBTOOL_SERIAL_H
+#define INC_USBTOOL_SERIAL_H
 
 //****************************************************************************
 // Include(s)
 //****************************************************************************
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <time.h>
-#include "flexsea.h"
-#include "flexsea_console.h"
-#include "flexsea_stream_log.h"
-#include "plan_spi.h"
-#include "plan_serial.h"
-#include "flexsea_local.h"
-#include "shuobot.h"
-#include "demo_test.h"
 
 //****************************************************************************
-// Public Function Prototype(s):
+// Prototype(s):
 //****************************************************************************
 
-
+void flexsea_serial_open(unsigned int tries, unsigned int delay);
+void flexsea_serial_close(void);
+void flexsea_serial_transmit(char bytes_to_send, unsigned char *serial_tx_data, unsigned char verbal);
+void flexsea_serial_putc(char *bytes_to_write);
 
 //****************************************************************************
 // Definition(s):
 //****************************************************************************
 
-#ifdef SINGLE_COMMAND
-#ifdef MULTIPLE_COMMANDS
-#error "Pick one Command option!"
-#endif
 #endif
 
-//Multiple commands:
-#define MAX_COMMAND_LEN 		256
-#define MAX_ARGS 				12
-
-
-#endif
