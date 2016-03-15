06c4d78cb77738ca16d881855eb44f24fd9e9dfa
Ubuntu 14 LTS <jfduval@aqra.ca>
2016-03-14 21:56:37 -0400

    Comm test code

--- flexsea-comm/src/flexsea.c
+++ flexsea-comm/src/flexsea.c
@@ -241,7 +241,7 @@ void rx_cmd_test(uint8_t *buf)
 			#ifdef BOARD_TYPE_FLEXSEA_EXECUTE
 
 			numb = tx_cmd_test(buf[P_XID], CMD_WRITE, tmp_payload_xmit, \
-								PAYLOAD_BUF_LEN, tmp_val1, mod);
+								PAYLOAD_BUF_LEN, mod, tmp_val2);
 			numb = comm_gen_str(tmp_payload_xmit, comm_str_485, numb);
 			numb = COMM_STR_BUF_LEN;
 			rs485_reply_ready(comm_str_485, numb);
@@ -256,7 +256,7 @@ void rx_cmd_test(uint8_t *buf)
 			#ifdef BOARD_TYPE_FLEXSEA_EXECUTE
 
 			numb = tx_cmd_test(buf[P_XID], CMD_WRITE, tmp_payload_xmit, \
-								PAYLOAD_BUF_LEN, 0xAA, 0);
+								PAYLOAD_BUF_LEN, 123, 234);
 			numb = comm_gen_str(tmp_payload_xmit, comm_str_485, numb);
 			numb = COMM_STR_BUF_LEN;
 			rs485_reply_ready(comm_str_485, numb);
